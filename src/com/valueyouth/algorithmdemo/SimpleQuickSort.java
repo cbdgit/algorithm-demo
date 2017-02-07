22debad091994c360b3795a9aaca9ec642469a5c
unknown <>
2017-02-07 23:06:56 +0800

    add the new algorithm SimpleQuickSort.java

---
+++ src/com/valueyouth/algorithmdemo/SimpleQuickSort.java
@@ -0,0 +1,31 @@
+package com.valueyouth.algorithmdemo;
+
+/**
+ * 类说明：简单快速排序算法
+ * @author cwang6
+ * @since 2017-2-7
+ * @version 1.0
+ */
+public class SimpleQuickSort
+{
+
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	
+	/**
+	 * 方法说明：
+	 * @param numbers
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	private int partition(int[] numbers, int left, int right)
+	{
+		
+		return 0;
+	}
+	
+}
